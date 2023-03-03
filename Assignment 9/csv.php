137220c17a94c20c027216998fb3675d0e32effe
geetansh-bhatnagar <geetansh.bhatnagar@sigmainfo.net>
2023-03-03 14:08:31 +0530

    commit

--- Assignment 9/csv.php
+++ Assignment 9/csv.php
@@ -1,17 +1,63 @@
 <?php
-echo '<table border="1">';
-$start_row = 1;
-if (($csv_file = fopen("airtravel.csv", "r")) !== FALSE) {
-  while (($read_data = fgetcsv($csv_file, 1000, ",")) !== FALSE) {
-    $column_count = count($read_data);
-	echo '<tr>';
-    $start_row++;
-    for ($c=0; $c < $column_count; $c++) {
-        echo "<td>".$read_data[$c] . "</td>";
+
+class FileUploader
+{
+    private $phpFile;
+    private $phpFileTmp;
+
+    public function __construct($phpFile, $phpFileTmp)
+    {  
+      // print_r(error_get_last());
+        $this->phpFile = $phpFile;
+        $this->phpFileTmp = $phpFileTmp;
     }
-	echo '</tr>';
-  }
-  fclose($csv_file);
+
+    public function displayTable()
+    {
+        
+
+        echo '<table border="1">';
+        $start_row = 1;
+        if (($csv_file = fopen($this->phpFileTmp, "r")) !== FALSE) {
+                 
+            while (($read_data = fgetcsv($csv_file, 1000, ",")) !== FALSE) {
+                $column_count = count($read_data);
+                echo '<tr>';
+                $start_row++;
+                for ($c = 0; $c < $column_count; $c++) {
+                    echo "<td>".$read_data[$c] . "</td>";
+                }
+                echo '</tr>';
+            }
+            fclose($csv_file);
+        }
+        echo '</table>';
+        echo "<br>";
+    }
+
+    public function updateCsv($newData)
+    {    echo "Updated table"."<br>"."<br>";
+        $fp = fopen($this->phpFileTmp, 'a');
+        fputcsv($fp, $newData);
+        fclose($fp);
+    }
 }
-echo '</table>';
+
+if (isset($_POST['submit'])) {
+
+    $phpFile = $_FILES['php_file']['name'];
+    
+    var_dump($phpFile);
+    $phpFileTmp = $_FILES['php_file']['tmp_name'];
+
+    $fileUploader = new FileUploader($phpFile, $phpFileTmp);
+    $fileUploader->displayTable();
+
+    $newData = array(4, 4, 4, 'Y');
+    $fileUploader->updateCsv($newData);
+
+    $fileUploader = new FileUploader($phpFile, $phpFileTmp);
+    $fileUploader->displayTable();
+
+}
 ?>
--- Assignment 9/index.php
+++ Assignment 9/index.php
@@ -18,10 +18,10 @@
     
   <div class="container"  >
   <h2>Reading and updating csv file using php</h2>
-<form action="/Php assignments/Assignment 9/csv.php" method="post">
+<form action="csv.php" method="post" enctype="multipart/form-data">
 <label for="myfile">Select a file:</label>
-<input type="file" id="myfile" name="myfile"> <br> <br>
-  <input type="submit" value="OPEN" style="background-color: yellow; border-color: aliceblue; border: 0px; width: 185px;
+<input type="file" id="php_file" name="php_file" accept=".csv"  > <br> <br>
+  <input type="submit" value="OPEN" name="submit" style="background-color: yellow; border-color: aliceblue; border: 0px; width: 185px;
     height: 41px; font-size: 25px;
     border-radius: 11px; cursor: pointer;" >
 </form> 
